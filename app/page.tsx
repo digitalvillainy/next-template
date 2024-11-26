29b170da22705553b71ffeff5b8c5c6e4f17a989
rrivera <rrivera@redbannermedia.com>
2024-11-26 13:27:42 -0500

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "Home Page Description",
+};
+
+export default function Home(): JSX.Element {
   return (
     <div>
       <h1 className="text-5xl font-bold text-red-500">
